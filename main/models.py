25da4b3e60fa61af249d1ae699c46229a2d7d713
Ilias <anotherthecharacter@gmail.com>
2022-12-16 12:05:03 +0600

    user

--- main/models.py
+++ main/models.py
@@ -1,12 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
-class Blogger(models.Model):
-    name = models.CharField(max_length=50)
+User = get_user_model()
 
 
 class Post(models.Model):
-    user_id = models.ForeignKey(Blogger, related_name='blogger', on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, related_name='blogger', on_delete=models.CASCADE)
     body = models.TextField()
     image = models.ImageField(upload_to='postsimg', null=True)
     created_at = models.DateField(auto_now_add=True)
