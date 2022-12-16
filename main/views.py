8c8f7d9778aad46597a26c177a7efe70b2661da0
eles <yryspaeva22@gmail.com>
2022-12-16 12:24:29 +0600

    ser + views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.viewsets import ModelViewSet
+from django.contrib.auth import get_user_model
+
+from .models import Post
+
+from .serializers import PostSerializers
+
+User = get_user_model()
+
+class PostViewSet(ModelViewSet):
+    queryset= Post.objects.all()
+    serializer_class = PostSerializers
