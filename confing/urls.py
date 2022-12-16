070bf7e79df6c3ba046f33fbcfa8bc809fe9fa59
Ilias <anotherthecharacter@gmail.com>
2022-12-16 12:38:13 +0600

    urls

--- confing/urls.py
+++ confing/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+
+from main.views import PostViewSet
+
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
 
 """=============Swagger docs============="""
 from drf_yasg import openapi
@@ -37,6 +44,8 @@ swagger_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', swagger_view.with_ui('swagger', cache_timeout=0)),
+    path('', include(router.urls)),
 ]
 
 
