5c34c0f8fe878c902e18e3fc04e0f77ef91c1c7b
Ilias <anotherthecharacter@gmail.com>
2022-12-16 11:59:53 +0600

    blogger + post models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Blogger(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Post(models.Model):
+    user_id = models.ForeignKey(Blogger, related_name='blogger', on_delete=models.CASCADE)
+    body = models.TextField()
+    image = models.ImageField(upload_to='postsimg', null=True)
+    created_at = models.DateField(auto_now_add=True)
